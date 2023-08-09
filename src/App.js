cde068e199782590040ba6198b05c5d21d594a12
ego <mainfhr@gmail.com>
2023-08-09 20:16:10 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import '../src/stileCSS/stiles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [count, setCount] = React.useState(0);
+
+   const plusOne = () => {
+      setCount(count + 1)
+   }
+   const minusOne = () => {
+      setCount(count - 1)
+   }
+
+   return (
+      <div className="App">
+      <div>
+         <h2>counter:</h2>
+         <h1>{count}</h1>
+         <button onClick={minusOne} className="minus">- minus</button>
+         <button onClick={plusOne} className="plus">plas +</button>
+      </div>
+      </div>
+   );
 }
 
 export default App;
